8f8f33cfcdc941688cd72920f6f9e8106efe89cc
Lynn Langit <lynnlangit@live.com>
2026-02-07 17:21:36 -0600

    implement v2

---
+++ v2/Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TestComponent() {
+    return (
+        <div className="p-10 bg-blue-500 text-white font-bold text-4xl">
+            Environment Check: SUCCESS
+        </div>
+    )
+}
